8db6c1ca7b123cb1a7dbb1a2ae5123baa3706cbf
Jarek <jaroslaw.czerwinski.it@gmail.com>
2023-01-10 08:03:55 +0100

    new price for extra milk

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -9,7 +9,7 @@ public final class Constant {
     public static final double LARGE_COFFEE_PRICE = 3.5;
     public static final double ORANGE_JUICE_PRICE = 3.95;
     public static final double BACON_ROLL_PRICE = 4.5;
-    public static final double EXTRA_MILK_PRICE = 0.3;
+    public static final double EXTRA_MILK_PRICE = 0.8;
     public static final double FOAMED_MILK_PRICE = 0.5;
     public static final double SPECIAL_ROAST_COFFEE_PRICE = 0.9;
 
